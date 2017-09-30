5aa4a36f201340c9e3e34ff25945464923bd8f84
96qbhy <96qbhy@gmail.com>
2017-09-30 16:34:03 +0800

    先忙其他事情

---
+++ game/data.go
@@ -0,0 +1,15 @@
+package game
+
+import (
+	"github.com/go-redis/redis"
+)
+
+var Client *redis.Client
+
+func init() {
+	Client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
